04d8571bb32e6b8358703cdd6fac52369528d93b
Eduardo CL <corteslalo97@gmail.com>
2022-11-09 19:08:24 -0800

    preliminary image gallery setup

--- components/imageGallery.js
+++ components/imageGallery.js
@@ -1,13 +1,35 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
+let one = "https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/logo_color.png?alt=media&token=bc69527a-09cf-48e5-8789-d861288f7443";
+let two= 'https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/karen.jpg?alt=media&token=b3acb614-81d1-470d-8983-a6b805b5388f';
+let tree = "https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/VBlocks_black-no_bg.png?alt=media&token=c30034e5-d515-4aa5-a21d-7a5869dbc184";
+
+const images = [one, two, tree];
 
 const Gallery = () => {
 
-    //"https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/logo_color.png?alt=media&token=bc69527a-09cf-48e5-8789-d861288f7443"
-    //'https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/karen.jpg?alt=media&token=b3acb614-81d1-470d-8983-a6b805b5388f'
-    return (
-        <div className="bg-white">
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(currentIndex === images.length - 1) {
+                setCurrentIndex(0);
+            } 
+            else {
+                 setCurrentIndex(currentIndex + 1);
+            }
+        }, 2000)
         
+        return () => clearInterval(intervalId);
+    }, [])
+
+
+    return (
+        <div>
+            <div>
+                <img className="mx-auto" src={images[currentIndex]} />
+            </div>
         </div>
     )
 }
