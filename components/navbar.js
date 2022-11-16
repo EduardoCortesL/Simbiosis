cb99d96bc325e4567ce6d81d29b59bdb64733d28
Eduardo CL <corteslalo97@gmail.com>
2022-11-16 13:21:31 -0800

    changed a for Link

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useState, useEffect } from "react"
 import {AiOutlineMenu} from 'react-icons/ai'
 
@@ -18,19 +19,19 @@ const NavBar = () => {
                             </button>
                             <ul className="absolute hidden pt-1 group-hover:block">
                                 <li>
-                                    <a className="rounded-t hover:text-black py-2 px-4 block whitespace-no-wrap" href="/about">ABOUT</a>
+                                    <Link className="rounded-t hover:text-black py-2 px-4 block whitespace-no-wrap" href="/about">ABOUT</Link>
                                 </li>
                                 <li>
-                                    <a className=" hover:text-black py-2 px-4 block whitespace-no-wrap" href="/projects">PROJECTS</a>
+                                    <Link className=" hover:text-black py-2 px-4 block whitespace-no-wrap" href="/projects">PROJECTS</Link>
                                 </li>
                                 <li>
-                                    <a className="rounded-b  hover:text-black py-2 px-4 block whitespace-no-wrap" href="/contact">CONTACT</a>
+                                    <Link className="rounded-b  hover:text-black py-2 px-4 block whitespace-no-wrap" href="/contact">CONTACT</Link>
                                 </li>
                             </ul>
                         </div>
-                        <a className="hidden md:flex hover:text-black transition-colors" href="/about">ABOUT</a>
-                        <a className="hidden md:flex hover:text-black transition-colors" href="/projects">PROJECTS</a>
-                        <a className="hidden md:flex hover:text-black transition-colors" href="/contact">CONTACT</a>
+                        <Link className="hidden md:flex hover:text-black transition-colors" href="/about">ABOUT</Link>
+                        <Link className="hidden md:flex hover:text-black transition-colors" href="/projects">PROJECTS</Link>
+                        <Link className="hidden md:flex hover:text-black transition-colors" href="/contact">CONTACT</Link>
                     </div>
             </div>
         </div>
