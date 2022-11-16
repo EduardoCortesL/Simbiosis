21ce2bebed32b3b607f70087d39777c7cbc74bdd
Eduardo CL <corteslalo97@gmail.com>
2022-11-16 13:22:41 -0800

    changed all <a> to Link

--- components/navbar.js
+++ components/navbar.js
@@ -9,9 +9,9 @@ const NavBar = () => {
     return (
         <div className="text-simbiosis-gray text-xs font-Roboto" >
             <div className="h-36 mx-auto px-12 flex items-center justify-around ">
-                <a className="relative hover:text-black transition-colors cursor-pointer" href="/">
+                <Link className="relative hover:text-black transition-colors cursor-pointer" href="/">
                     <img className="h-5" src={"https://firebasestorage.googleapis.com/v0/b/bito-b4105.appspot.com/o/Simbiosis%2FLogo-removebg-preview.png?alt=media&token=d3294d5c-571f-4bef-a907-2ac6fd987d31"}></img>
-                </a>
+                </Link>
                     <div className="flex items-center gap-5 ">
                         <div className="md:hidden group flex inline-block relative">
                             <button className="inline-flex items-center hover:text-black">
