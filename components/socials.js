dfe6bb4415a0872ccebf75507ab0653ef9851d00
Eduardo CL <corteslalo97@gmail.com>
2024-09-30 21:58:42 -0700

    fixed socials on dekstop

--- components/socials.js
+++ components/socials.js
@@ -8,9 +8,9 @@ import Link from "next/link";
 
 const Socials = () => {
     return (
-        <div className='flex flex-col justify-center items-center md:mb-8'>
-            <div className='flex flex-col gap-2 text-center text-white font-poppins text-xs md:text-sm'>
-                <Link href="https://www.instagram.com/simbiosis_estudiomx/">@simbiosis_estudiomx</Link>
+        <div className='flex justify-center items-center md:mb-8'>
+            <div className='flex flex-col md:flex-row gap-2 text-center text-white font-poppins text-xs md:text-sm'>
+                <Link href="https://www.instagram.com/simbiosis_estudiomx/">simbiosis_estudiomx</Link>
                 <Link href="mailto:info@simbiosis-estudio.com">info@simbiosis-estudio.com</Link>
             </div>
         </div>
@@ -18,6 +18,7 @@ const Socials = () => {
 }
 
 export default Socials;
+
 
 
 
